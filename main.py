4ecca5974e3a4fb17f33c8097dea3d81b6fee055
BanguelaDev <57540511+firexytbr@users.noreply.github.com>
2024-10-17 00:53:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ jogador = {
     'experiência': 0,
     'pontos de vida': 3,
     
-    'atributos': { # sei la
-        'força': 3, # Vc pediu pra deixar tudo em 1, mas no desafio dos monstros o personagem tem q ter 3 de força..
+    'atributos': {
+        'força': 3, # O professor pediu pra deixar tudo em 1, mas no desafio dos monstros o personagem tem q ter 3 de força..
         'defesa': 1,
         'agilidade': 1,
         'inteligencia': 1,
