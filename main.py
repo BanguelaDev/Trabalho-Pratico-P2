3f87e12a85da67e730650106cf32543fe4db6bfb
Matheus Ferreira Silva <matheusfs160@gmail.com>
2024-11-04 15:03:50 -0300

    Update main.py
    Atualizei alguns detalhes, add route do teste de ataque e add o drope dos monstros

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 import random
+import copy
 
 app = Flask(__name__)
 app.secret_key = 'tads'
@@ -27,40 +28,41 @@ races = {
 
 # to fazendo aqueles requisitos inicias que o professor mandou fazer, o sistema e os testes e assim adiante
 
+monstros = {
+    'fraco': {'ataque':3, 'defesa':1, 'vida':8, 'esquiva':2},
+    'medio': {'ataque':4, 'defesa':1, 'vida':12, 'esquiva':4,},
+    'dificil': {'ataque':6, 'defesa':2, 'vida':20, 'esquiva':6,},
+    'chefe': {'ataque':10, 'defesa':5, 'vida':45, 'esquiva':8,},
+}
 # o sistema de D20
 def D20():
     return random.randint(1,20)
+
+def escolha_do_monstro():# usando o D100 pra ver qual monstro vai dropar 
+    drope = random.randint(0,100)
+    if drope <= 39:
+        return monstros['fraco']
+    elif drope <= 69:
+        return monstros['medio']
+    elif drope <=89:
+        return monstros['dificil']
+    else:
+        return monstros['chefe']
 
 # teste ataque (pedido de socorro: to entendendo nada)
 # depois lanço o english cabuloso, se não vou me confundir 
 def teste_de_ataque(ataque_usuario, defesa_inimigo):
     rolar_dado = D20()  
-    ataque_final = rolar_dado + ataque_usuario  
-    CRITICO = rolar_dado == 20 # isso aqui é em relação ao dano critico
-
-    if ataque_final >= defesa_inimigo:
-        return True, CRITICO 
-    return False, False 
+    ataque_final = rolar_dado + ataque_usuario
+    critico = rolar_dado == 20  
+    return ataque_final >= defesa_inimigo, critico
 
 # calcular o dano
 def calcular_dano(ataque_usuario, defesa_inimigo, dano_critico):
-    dano_inicial = max(0, ataque_usuario - defesa_inimigo )
+    dano_inicial = max(0, ataque_usuario - defesa_inimigo)
     if dano_critico:
-        return dano_inicial * 2 
+        return dano_inicial * 2
     return dano_inicial
-
-monstros = {
-    'fraco': {'ataque:3,' 'defesa:1,' 'vida:8,' 'esquiva:2'},
-    'medio': {'ataque:4,' 'defesa:1,' 'vida:12,' 'esquiva:4,'},
-    'dificil': {'ataque:6,' 'defesa:2,' 'vida:20,' 'esquiva:6,'},
-    'chefe': {'ataque:10,' 'defesa:5,' 'vida:45,' 'esquiva:8,'},
-}
-# tinha esquecido da route
-@app.route("/teste_de_ataque", methods=['POST'])
-def ataque_route():
-    username = session.get('username')
-    if not username:
-        return redirect(url_for('homepage'))
 
 # Simulando um banco de dados com um dicionário, trocar por sql (não faço ideia de como funciona)
 accounts = {}
@@ -81,7 +83,7 @@ def loginRegister():
         else:
             return "Senha incorreta! Tente novamente.", 401
     else:
-        accounts[username] = {'password': password, 'player': player_template.copy()}
+        accounts[username] = {'password': password, 'player': copy.deepcopy(player_template)}
         accounts[username]['player']['name'] = username
         session['username'] = username
         return redirect(url_for('ticketRegister'))
@@ -111,6 +113,37 @@ def ticket():
             return render_template("ticket.html", player=player_info)
     
     return redirect(url_for('homepage'))
+
+# tinha esquecido da route
+@app.route("/teste_de_ataque", methods=['POST'])
+def ataque_route():
+    username = session.get('username')
+    if not username:
+        return redirect(url_for('homepage'))
+    
+    player = accounts[username][player] if username in accounts else None
+    monstros = escolha_do_monstro()
+
+    if not player or not monstros:
+        return "Erro ao carregar dados do player ou monstro.", 500
+    
+    dano = 0
+    
+    ataque, critico = teste_de_ataque(player['attributes']['attack'], monstros['defesa'])
+    if ataque:
+        dano = calcular_dano(player['attributes']['attack'], monstros['defesa'], critico)
+        resultado = f"Ataque Bem-Sucedido!!! {'Dano Crítico!!!' if critico else 'Dano Normal.'} Dano causado: {dano}"
+    else:
+        resultado = "O ataque falhou"
+
+    return render_template("resultado_ataque.html", resultado = resultado)  
+    
+
+
+
+    
+    
+    
 
 
 if __name__ == "__main__":
