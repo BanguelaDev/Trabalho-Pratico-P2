7c865ccdfb41794554e734e32b62d74661d9d433
BanguelaDev <57540511+firexytbr@users.noreply.github.com>
2024-11-09 13:32:20 -0400

    .
    .

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        'medieval-gold': '#f7c531',
+        'dark-gray': '#2c2c2c',
+        'scroll-bg': '#3e3e3e',
+        'scroll-border': '#f5f5f5',
+        'scroll-shadow': '#000000'
+      },
+    },
+  },
+  plugins: [],
+}
